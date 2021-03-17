16a07871ee0cc78e7d38cac3daa3f4fd99291916
clearloop <udtrokia@gmail.com>
2021-03-17 16:54:17 +0800

    Link wasmtime with coremark

--- main.rs
+++ main.rs
@@ -1,14 +1,35 @@
-trait Bench {
-    /// Load wat bytes and new instance
-    fn new(b: &[u8]) -> Self;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-    /// Register env
-    fn register(&mut self);
+/// Wasmtime coremark
+fn wasmtime_coremark(b: &[u8]) {
+    use wasmtime::{Linker, Module, Store};
 
-    /// Run the coremark
-    fn run(&self);
+    let store = Store::default();
+    let mut linker = Linker::new(&store);
+
+    linker
+        .func("env", "clock_ms", || {
+            SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Clock may have gone backwards")
+                .as_millis() as i32
+        })
+        .expect("Link clock_ms failed");
+
+    let res = linker
+        .instantiate(
+            &Module::new(store.engine(), b).expect("Init wasm module failed in wasmtime coremark"),
+        )
+        .expect("Link module core-mark failed")
+        .get_func("run")
+        .expect("Could not find function `run` in the coremark")
+        .call(&[])
+        .expect("failed running coremark in wasmtime");
+
+    println!("{:?}", res);
 }
 
 fn main() {
-    println!("Hello, world");
+    let bytes = include_bytes!("coremark-minimal.wat");
+    wasmtime_coremark(bytes);
 }
